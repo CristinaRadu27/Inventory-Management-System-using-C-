41501edf3b73eb61af26210d78718058b21d1907
CristinaRadu27 <88380900+CristinaRadu27@users.noreply.github.com>
2021-08-03 14:57:16 +0300

    Add files via upload

---
+++ Home.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagement
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ManageProducts prod = new ManageProducts();
+            prod.Show();
+            this.Hide();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            ManageCategories cat = new ManageCategories();
+            cat.Show();
+            this.Hide();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            ManageUsers user = new ManageUsers();
+            user.Show();
+            this.Hide();
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            ManageCustomers customers = new ManageCustomers();
+            customers.Show();
+            this.Hide();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            ManageOrders order = new ManageOrders();
+            order.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 login = new Form1();
+            login.Show();
+            this.Hide();
+        }
+    }
+}
